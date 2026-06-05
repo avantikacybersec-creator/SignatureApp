434e34dc857cdd125a6fe8442203f4087d421fd8
Avantika Tagde <avantikacybersec@gmail.com>
2026-06-05 16:06:36 +0530

    Day 1: Project setup and database configuration

---
+++ signatureapp/src/main/java/com/signature/signatureapp/SignatureappApplication.java
@@ -0,0 +1,13 @@
+package com.signature.signatureapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignatureappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignatureappApplication.class, args);
+	}
+
+}
