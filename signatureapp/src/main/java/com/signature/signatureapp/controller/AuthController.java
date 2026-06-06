a8bbdcedcca77db21ab79b483d59adfa91519750
Avantika Tagde <avantikacybersec@gmail.com>
2026-06-06 16:25:25 +0530

    Day 2 : Completed

---
+++ signatureapp/src/main/java/com/signature/signatureapp/controller/AuthController.java
@@ -0,0 +1,25 @@
+package com.signature.signatureapp.controller;
+
+
+
+import com.signature.signatureapp.dto.RegisterRequest;
+import com.signature.signatureapp.model.User;
+import com.signature.signatureapp.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public User register(@RequestBody RegisterRequest request) {
+
+        return userService.register(request);
+    }
+}
---
+++ signatureapp/src/main/java/com/signature/signatureapp/service/UserService.java
@@ -0,0 +1,34 @@
+package com.signature.signatureapp.service;
+
+import com.signature.signatureapp.dto.RegisterRequest;
+import com.signature.signatureapp.model.User;
+import com.signature.signatureapp.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository,
+                       BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User register(RegisterRequest request) {
+
+        User user = new User();
+
+        user.setName(request.getName());
+        user.setEmail(request.getEmail());
+
+        user.setPassword(
+                passwordEncoder.encode(request.getPassword())
+        );
+
+        return userRepository.save(user);
+    }
+}
